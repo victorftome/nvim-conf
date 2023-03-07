2cb8d75ed93f31e341b782e8e8d1d69514f10145
victorftome <victorforift@gmail.com>
2023-03-07 15:24:36 +0100

    a

--- init.lua
+++ init.lua
@@ -23,7 +23,7 @@ require("nvim-tree").setup({
         group_empty = true,
     },
     filters = {
-        dotfiles = true
+        dotfiles = false
     },
 })
 
@@ -43,12 +43,8 @@ local cmp = require 'cmp'
 
 cmp.setup({
     snippet = {
-        -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+            vim.fn["vsnip#anonymous"](args.body)
         end
     },
     window = {
@@ -62,17 +58,13 @@ cmp.setup({
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({
             select = true
-        }) -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        })
     }),
     sources = cmp.config.sources({{
         name = 'nvim_lsp'
     }, {
         name = 'vsnip'
-    } -- For vsnip users.
-    -- { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
-    }, {{
+    }}, {{
         name = 'buffer'
     }})
 })
