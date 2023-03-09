60c9b4dc968a177707ad1adc7c3ed90ec870662f
victorftome <victorforift@gmail.com>
2023-03-09 17:14:21 +0100

    xd

--- init.lua
+++ init.lua
@@ -13,7 +13,16 @@ require'nvim-treesitter.configs'.setup {
 
 -- Keymaps, 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', 'f', builtin.find_files, {})
+local M = {}
+
+M.find_files = function()
+    builtin.find_files {
+        find_command = {'rg', '--files', '--iglob', '!.git', '--hidden'},
+        previewer = false
+    }
+end
+
+vim.keymap.set('n', 'f', M.find_files, {})
 vim.keymap.set('n', 'g', builtin.live_grep, {})
 
 -- nvim-tree
