d2d6ff06bbc5e20184549c4ad7841bbf57eb9e4c
victorftome <victorforift@gmail.com>
2023-03-26 12:36:50 +0200

    null-ls

--- lua/null-ls-config/init.lua
+++ lua/null-ls-config/init.lua
@@ -3,9 +3,9 @@ local null_ls = require("null-ls")
 local formatting = null_ls.builtins.formatting
 
 local sources = {
-    formatting.clang_format,
-    formatting.rustfmt,
-    formatting.stylua,
+	formatting.clang_format.with({ extra_args = { "-style=", "{BasedOnStyle: Microsoft, IndentWidth: 4}" } }),
+	formatting.rustfmt,
+	formatting.stylua,
 }
 
 null_ls.setup({ sources = sources })
