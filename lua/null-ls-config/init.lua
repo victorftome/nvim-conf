1c8ed4ce94029480898c8ef2da82c8fe86ab433d
victorftome <victorforift@gmail.com>
2023-03-26 23:26:45 +0200

    format on save

--- lua/null-ls-config/init.lua
+++ lua/null-ls-config/init.lua
@@ -6,6 +6,27 @@ local sources = {formatting.clang_format.with({
     extra_args = {"--style={BasedOnStyle: Microsoft, UseTab: Always, IndentWidth: 4, BreakBeforeBraces: Allman, AllowShortIfStatementsOnASingleLine: false, IndentCaseLabels: false, ColumnLimit: 0}"}
 }), formatting.rustfmt, formatting.stylua}
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
-    sources = sources
+    sources = sources,
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({
+                group = augroup,
+                buffer = bufnr
+            })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.format({
+                        bufnr = bufnr,
+                        filter = function(client)
+                            return client.name == "null-ls"
+                        end
+                    })
+                end
+            })
+        end
+    end
 })
