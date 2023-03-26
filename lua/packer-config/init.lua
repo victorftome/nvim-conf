3f739872016e079d9e521372765bf8f3af7e322c
victorftome <victorforift@gmail.com>
2023-03-26 23:44:35 +0200

    zen mode

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -1,73 +1,87 @@
-return require('packer').startup({function(use)
+return require("packer").startup({
+	function(use)
+		use("wbthomason/packer.nvim")
 
-    use('wbthomason/packer.nvim')
+		use("nvim-lua/plenary.nvim")
 
-    use('nvim-lua/plenary.nvim')
+		use({
+			"nvim-treesitter/nvim-treesitter",
+			run = ":TSUpdate",
+		})
 
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
+		use("navarasu/onedark.nvim")
 
-    use 'navarasu/onedark.nvim'
+		use({
+			"catppuccin/nvim",
+			as = "catppuccin",
+		})
 
-    use {
-        "catppuccin/nvim",
-        as = "catppuccin"
-    }
+		use({
+			"nvim-telescope/telescope.nvim",
+			tag = "0.1.x",
+			requires = { { "nvim-lua/plenary.nvim" } },
+		})
 
-    use {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.x',
-        requires = {{'nvim-lua/plenary.nvim'}}
-    }
+		use({
+			"nvim-tree/nvim-tree.lua",
+			requires = {
+				"nvim-tree/nvim-web-devicons", -- optional, for file icons
+			},
+		})
 
-    use {
-        'nvim-tree/nvim-tree.lua',
-        requires = {'nvim-tree/nvim-web-devicons' -- optional, for file icons
-        }
-    }
+		use({
+			"tanvirtin/vgit.nvim",
+			requires = { "nvim-lua/plenary.nvim" },
+		})
 
-    use {
-        'tanvirtin/vgit.nvim',
-        requires = {'nvim-lua/plenary.nvim'}
-    }
+		use({
+			"nvim-lualine/lualine.nvim",
+			requires = {
+				"kyazdani42/nvim-web-devicons",
+				opt = true,
+			},
+		})
 
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = {
-            'kyazdani42/nvim-web-devicons',
-            opt = true
-        }
-    }
+		use("neovim/nvim-lspconfig")
 
-    use 'neovim/nvim-lspconfig'
+		use({
+			"hrsh7th/nvim-cmp",
+			requires = {
+				"hrsh7th/cmp-buffer",
+				"hrsh7th/cmp-nvim-lsp",
+				"hrsh7th/cmp-nvim-lua",
+				"hrsh7th/vim-vsnip",
+				"octaltree/cmp-look",
+				"hrsh7th/cmp-path",
+				"hrsh7th/cmp-calc",
+				"f3fora/cmp-spell",
+				"hrsh7th/cmp-emoji",
+			},
+		})
 
-    use {
-        'hrsh7th/nvim-cmp',
-        requires = {'hrsh7th/cmp-buffer', 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-nvim-lua', 'hrsh7th/vim-vsnip',
-                    'octaltree/cmp-look', 'hrsh7th/cmp-path', 'hrsh7th/cmp-calc', 'f3fora/cmp-spell',
-                    'hrsh7th/cmp-emoji'}
-    }
+		use({
+			"romgrk/barbar.nvim",
+			requires = "nvim-web-devicons",
+		})
 
-    use {
-        'romgrk/barbar.nvim',
-        requires = 'nvim-web-devicons'
-    }
+		use({
+			"akinsho/toggleterm.nvim",
+			tag = "*",
+			config = function()
+				require("toggleterm").setup()
+			end,
+		})
 
-    use {
-        "akinsho/toggleterm.nvim",
-        tag = '*',
-        config = function()
-            require("toggleterm").setup()
-        end
-    }
+		use({
+			"jose-elias-alvarez/null-ls.nvim",
+			config = function()
+				require("null-ls").setup()
+			end,
+			requires = { "nvim-lua/plenary.nvim" },
+		})
 
-    use {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-            require("null-ls").setup()
-        end,
-        requires = {"nvim-lua/plenary.nvim"}
-    }
-end})
+		use({
+			"folke/zen-mode.nvim",
+		})
+	end,
+})
