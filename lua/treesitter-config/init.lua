871bfe5664a9d1de23d9c47a9c5b29322ce4ef28
victorftome <victorforift@gmail.com>
2023-04-11 19:38:37 +0200

    x

--- lua/treesitter-config/init.lua
+++ lua/treesitter-config/init.lua
@@ -1,5 +1,19 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "c", "lua", "help", "query", "sql", "rust", "bash", "vim", "toml", "ron", "python" },
+	ensure_installed = {
+		"c",
+		"lua",
+		"help",
+		"query",
+		"sql",
+		"rust",
+		"bash",
+		"vim",
+		"toml",
+		"ron",
+		"python",
+		"make",
+		"cmake",
+	},
 	sync_install = false,
 	auto_install = true,
 	highlight = {
