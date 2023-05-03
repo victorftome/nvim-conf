78a5a119f44cc597c6c9b08f6cb1386d3adefb1b
victorftome <victorforift@gmail.com>
2023-05-03 16:07:31 +0200

    js

--- lua/null-ls-config/init.lua
+++ lua/null-ls-config/init.lua
@@ -16,7 +16,7 @@ local sources = {
 	formatting.stylua,
 	formatting.autopep8,
 	formatting.beautysh.with({ extra_args = { "--tab" } }),
-	formatting.eslint,
+	formatting.eslint_d,
 }
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
