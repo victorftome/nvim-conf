88069ba3646d884c65f595c8d56409193daa3979
victorftome <victorforift@gmail.com>
2025-01-24 21:06:43 +0100

    ts_ls

--- lua/lspconfig-config/init.lua
+++ lua/lspconfig-config/init.lua
@@ -1,6 +1,6 @@
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-local langservers = { "clangd", "pyright", "bashls", "tsserver", "rust_analyzer", "html" }
+local langservers = { "clangd", "pylsp", "bashls", "ts_ls", "rust_analyzer", "html" }
 
 for _, server in ipairs(langservers) do
 	require("lspconfig")[server].setup({
